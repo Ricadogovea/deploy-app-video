bec27eae72c90f841a5e9799d4cd4615a6299cc4
Ricardo Govea López <hpricardogovea@hotmail.com>
2020-07-25 19:53:11 -0500

    Updates

---
+++ precache-manifest.bff0ee0338331addbdbed437046d5251.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc1e6e2f70480c07616b8f6f27e7744",
+    "url": "/Ricadogovea/deploy-app-video.git/index.html"
+  },
+  {
+    "revision": "f812267657263655f902",
+    "url": "/Ricadogovea/deploy-app-video.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/Ricadogovea/deploy-app-video.git/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ricadogovea/deploy-app-video.git/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f812267657263655f902",
+    "url": "/Ricadogovea/deploy-app-video.git/static/js/main.9e926bf2.chunk.js"
+  },
+  {
+    "revision": "80b66763c8dc0f514b91",
+    "url": "/Ricadogovea/deploy-app-video.git/static/js/runtime-main.418f3233.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Ricadogovea/deploy-app-video.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
